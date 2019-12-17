2013d1a49b0edf8cdb9ab681a441457c486804ae
Lukasz <lys.lukasz555@gmail.com>
2019-12-17 21:21:06 +0100

    init

---
+++ src/main/java/com/example/demo/domain/Customer/CustomerEndpoint.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain.Customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/customer")
+public class CustomerEndpoint {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerEndpoint(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping(path = "/getCustomers")
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    @GetMapping(path = "/add")
+    public Customer addCustomer(){
+        Customer customer = new Customer();
+        customer.firstName = "jacek";
+        customer.lastName = "placek";
+        return customerRepository.save(customer);
+    }
+}
